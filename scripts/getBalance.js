f91dae920479161304bc836d7a09976479ded970
Abhay Kejriwal <167509423+AbhayKejriwal04@users.noreply.github.com>
2024-08-31 23:51:49 +0530

    Create getBalance.js

---
+++ scripts/getBalance.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+const tokenContractJSON = require("../artifacts/contracts/travl.sol/travl.json");
+
+const tokenAddress = "0x2D2f41BfBa352C388aeAF2A74a09F5Adde11b81F"; // place your erc20 contract address here
+const tokenABI = tokenContractJSON.abi;
+const walletAddress = "0x28D1759B13CD4fA182d18E2Be3D4A5DdBEE6920F"; // place your public address for your wallet here
+
+async function main() {
+
+    const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+
+    console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
+  }
+  
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
